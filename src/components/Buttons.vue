018eb31471eaae57f6031175877f0a4c93b895d1
Megumi Mochizuki <mochizuki@rista.jp>
2017-07-12 21:38:25 +0900

    コンポーネントにした

---
+++ src/components/Buttons.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <div class="buttons">
+      <div class="item">
+        <!-- <button class="right js_right_button" @click.stop="moveFlag('right')">↑</button> -->
+        <button class="right js_right_button">↑</button>
+      </div>
+      <div class="item">
+        <!-- <button class="left js_left_button" @click.stop="moveFlag('left')">↑</button> -->
+        <button class="left js_left_button">↑</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'buttons'
+    // methods: {
+    //   moveFlag (hand) {
+    //     let buttunClass = '.js_' + hand + '_button'
+    //     let button = document.querySelector(buttunClass)
+    //     let flagClass = '.js_' + hand + '_flag'
+    //     let flag = document.querySelector(flagClass)
+    //     this.changeButton(button)
+    //     this.changeFlag(flag)
+    //   },
+    //   changeButton (button) {
+    //     if (button.classList.contains('raise')) {
+    //       button.innerHTML = '↑'
+    //       button.classList.remove('raise')
+    //     } else {
+    //       button.innerHTML = '↓'
+    //       button.classList.add('raise')
+    //     }
+    //   },
+    //   changeFlag (flag) {
+    //     if (flag.classList.contains('raise')) {
+    //       flag.classList.remove('raise')
+    //     } else {
+    //       flag.classList.add('raise')
+    //     }
+    //   }
+    // }
+}
+</script>
+
+<style lang="scss">
+  .buttons {
+    overflow: hidden;
+    width: 315px;
+    margin: 0 auto;
+    padding: 30px 0;
+
+    .item {
+      width: 50%;
+      float: left;
+      text-align: center;
+    }
+
+    button {
+      cursor: pointer;
+      outline: none;
+      appearance: none;
+      padding: 10px;
+      line-height: 1;
+      font-size: 24px;
+      font-weight: 700;
+      border-radius: 50%;
+    }
+
+    .right {
+      background-color: #e20000;
+      border: 2px solid #af0202;
+      color: #fff;
+    }
+
+    .left {
+      background-color: #fff;
+      border: 2px solid #afafaf;
+      color: #000;
+    }
+  }
+</style>
---
+++ src/components/Polidog.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <div class="polidog">
+      <div class="right js_right_flag">
+      </div>
+      <div class="left js_left_flag">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'polidog'
+    // methods: {
+    //   moveFlag (hand) {
+    //     let buttunClass = '.js_' + hand + '_button'
+    //     let button = document.querySelector(buttunClass)
+    //     let flagClass = '.js_' + hand + '_flag'
+    //     let flag = document.querySelector(flagClass)
+    //     this.changeButton(button)
+    //     this.changeFlag(flag)
+    //   },
+    //   changeButton (button) {
+    //     if (button.classList.contains('raise')) {
+    //       button.innerHTML = '↑'
+    //       button.classList.remove('raise')
+    //     } else {
+    //       button.innerHTML = '↓'
+    //       button.classList.add('raise')
+    //     }
+    //   },
+    //   changeFlag (flag) {
+    //     if (flag.classList.contains('raise')) {
+    //       flag.classList.remove('raise')
+    //     } else {
+    //       flag.classList.add('raise')
+    //     }
+    //   }
+    // }
+  }
+</script>
+
+<style lang="scss">
+  .polidog {
+    overflow: hidden;
+    width: 315px;
+    height: 315px;
+    margin: 0 auto;
+    padding: 30px 0;
+
+    .right,
+    .left {
+      width: 50%;
+      float: left;
+      height: 0;
+      padding-top: 315px;
+    }
+
+    .right {
+      background-image: url('../../static/img/polidog_flag_right.png');
+      background-repeat: no-repeat;
+      background-position: 2px center;
+      background-size: cover;
+
+      &.raise {
+        background-image: url('../../static/img/polidog_flag_raise_right.png');
+        background-position: 0 center;
+      }
+    }
+
+    .left {
+      background-image: url('../../static/img/polidog_flag_left.png');
+      background-repeat: no-repeat;
+      background-size: cover;
+
+      &.raise {
+        background-image: url('../../static/img/polidog_flag_raise_left.png');
+      }
+    }
+  }
+</style>
